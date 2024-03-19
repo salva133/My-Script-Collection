fa2890c39f122cbc82c9880b52b80b42c9f83f34
salva133 <c.santana@headbeat.de>
2024-03-19 23:07:54 +0100

    Update json2yaml.py

--- py/json2yaml.py
+++ py/json2yaml.py
@@ -1,3 +1,14 @@
+"""
+Script Name: json2yaml.py
+Description: This script converts a JSON file into a YAML file. It is designed to facilitate data transformation between these two popular formats, making it easier to work with configuration files, data exchange between languages, or for use in applications that require YAML format.
+
+Usage:
+    - The script currently converts 'list-pharmacies.json' to 'list-pharmacies.yml'.
+    - To convert a different JSON file, replace 'list-pharmacies.json' with the path to the desired file.
+    - The output YAML will be named 'list-pharmacies.yml', but this can be changed to any desired output file name.
+"""
+
+
 import yaml
 import json
 
