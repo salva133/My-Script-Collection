41d52ecab49f88737e8d4c06fb03b7b9354c5da2
salva133 <c.santana@headbeat.de>
2025-01-31 20:47:35 +0100

    Create encrypt_vkey.py

---
+++ py/encrypt_vkey.py
@@ -0,0 +1,20 @@
+from cryptography.fernet import Fernet
+from vkey import KEY
+
+# SECRET_KEY aus Datei laden oder manuell einfügen
+with open("secret.key", "rb") as key_file:
+    SECRET_KEY = key_file.read()
+
+cipher = Fernet(SECRET_KEY)
+
+# Ersetze dies mit deinem tatsächlichen Vigenère-Schlüssel
+VKEY = KEY
+
+# VKEY verschlüsseln
+encrypted_vkey = cipher.encrypt(VKEY.encode())
+
+# In Datei speichern
+with open("vkey.enc", "wb") as f:
+    f.write(encrypted_vkey)
+
+print("VKEY wurde erfolgreich verschlüsselt und gespeichert.")
