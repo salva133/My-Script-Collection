01612265ff9ec941cc413e8d4c61ecd54f34351b
Mark <34437542+salva133@users.noreply.github.com>
2025-09-16 17:55:21 +0200

    Update audio_transfer.bat

--- bat/audio_transfer.bat
+++ bat/audio_transfer.bat
@@ -1,6 +1,6 @@
 @echo off
 
-call C:\Users\Asus\Desktop\GitHub\My-Script-Collection\py\venv\Scripts\activate.bat
+call C:\Users\Asus\Desktop\GitHub\My-Script-Collection\py\.venv\Scripts\activate.bat
 cd /d C:\Users\Asus\Desktop\GitHub\My-Script-Collection\py
 py transfer_audio_to_cloud.py
 
